eec57cb4a1e66b3a13145242183a2b85d7d5bb7f
PrafulGupta <guptapraful130@gmail.com>
2025-06-12 23:01:11 +0530

    backend update

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -17,36 +17,21 @@ export const postJob = catchAsyncErrors(async (req, res, next) => {
       new ErrorHandler("Job Seeker not allowed to access this resource.", 400)
     );
   }
-  const {
-    title,
-    description,
-    category,
-    country,
-    city,
-    location,
-    fixedSalary,
-    salaryFrom,
-    salaryTo,
-  } = req.body;
+  const { title, description, category, country, city, location, salary } =
+    req.body;
 
-  if (!title || !description || !category || !country || !city || !location) {
+  if (
+    !title ||
+    !description ||
+    !category ||
+    !country ||
+    !city ||
+    !location ||
+    !salary
+  ) {
     return next(new ErrorHandler("Please provide full job details.", 400));
   }
 
-  if ((!salaryFrom || !salaryTo) && !fixedSalary) {
-    return next(
-      new ErrorHandler(
-        "Please either provide fixed salary or ranged salary.",
-        400
-      )
-    );
-  }
-
-  if (salaryFrom && salaryTo && fixedSalary) {
-    return next(
-      new ErrorHandler("Cannot Enter Fixed and Ranged Salary together.", 400)
-    );
-  }
   const postedBy = req.user._id;
   const job = await Job.create({
     title,
@@ -55,9 +40,7 @@ export const postJob = catchAsyncErrors(async (req, res, next) => {
     country,
     city,
     location,
-    fixedSalary,
-    salaryFrom,
-    salaryTo,
+    salary,
     postedBy,
   });
   res.status(200).json({
--- backend/models/jobSchema.js
+++ backend/models/jobSchema.js
@@ -30,17 +30,7 @@ const jobSchema = new mongoose.Schema({
     required: [true, "Please provide location."],
     minLength: [5, "Location must contian at least 20 characters!"],
   },
-  fixedSalary: {
-    type: Number,
-    minLength: [4, "Salary must contain at least 4 digits"],
-    maxLength: [9, "Salary cannot exceed 9 digits"],
-  },
-  salaryFrom: {
-    type: Number,
-    minLength: [4, "Salary must contain at least 4 digits"],
-    maxLength: [9, "Salary cannot exceed 9 digits"],
-  },
-  salaryTo: {
+  salary: {
     type: Number,
     minLength: [4, "Salary must contain at least 4 digits"],
     maxLength: [9, "Salary cannot exceed 9 digits"],
